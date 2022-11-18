7cebf08e9e6149fb2d0f4557dc1449bd72de2a95
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-11-18 15:36:01 +0530

    Fetching Game details, adding game deatils to site, adding styling

--- src/api.js
+++ src/api.js
@@ -38,3 +38,10 @@ const new_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&orderin
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+
+// Game Details
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${API_KEY}`;
+// Game Screenshots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${API_KEY}`;
